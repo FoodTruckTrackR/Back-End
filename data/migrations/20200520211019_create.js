7c61fd40ceecd38a30180696f62ff1b6536cf32b
tlewandowski18 <tlewandowski18@gmail.com>
2020-05-20 23:06:35 -0400

    create tables in first migration

---
+++ data/migrations/20200520211019_create.js
@@ -0,0 +1,91 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable("operators", table => {
+      table.increments()
+      table.text("username").notNull().unique()
+      table.text("password").notNull()
+      table.text("userType").defaultTo("operator")
+  })
+
+  await knex.schema.createTable("trucks", table => {
+      table.increments()
+      table.text("imageOfTruck").notNull().unique()
+      table.text("cuisineType")
+      table.integer("operatorId")
+        .notNull()
+        .references("id")
+        .inTable("operators")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+  })
+
+  await knex.schema.createTable("menu-items", table => {
+    table.increments()
+    table.text("itemName").notNull()
+    table.text("itemDescription").notNull()
+    table.text("itemPhoto").notNull()
+    table.float("itemPrice").notNull()
+    table.integer("truckId")
+      .notNull()
+      .references("id")
+      .inTable("trucks")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+  })
+
+  await knex.schema.createTable("ratings", table => {
+    table.increments()
+    table.integer("rating").notNull()
+    table.integer("menuId")
+      .notNull()
+      .references("id")
+      .inTable("menu-items")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+  })
+
+  await knex.schema.createTable("locations", table => {
+    table.increments()
+    table.text("location").notNull()
+    table.text("arrivalTime")
+    table.text("departureTime")
+    table.integer("truckId")
+      .references("id")
+      .inTable("trucks")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+  })
+  
+  await knex.schema.createTable("diners", table => {
+    table.increments()
+    table.text("username").notNull().unique()
+    table.text("password").notNull()
+    table.text("currentLocation").notNull()
+    table.text("userType").defaultTo("diner")
+  })
+
+  await knex.schema.createTable("diners-trucks", table => {
+    table.integer("dinerId")
+      .references("id")
+      .inTable("diners")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE")
+    table.integer("truckId")
+      .references("id")
+      .inTable("trucks")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE")
+    table.primary(["dinerId", "truckId"])
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("diners-trucks")
+  await knex.schema.dropTableIfExists("diners")
+  await knex.schema.dropTableIfExists("ratings")
+  await knex.schema.dropTableIfExists("locations")
+  await knex.schema.dropTableIfExists("menu-items")
+  await knex.schema.dropTableIfExists("trucks")
+  await knex.schema.dropTableIfExists("operators")
+
+};
