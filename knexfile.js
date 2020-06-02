9a0b995d4fede1870a6dc50490358eb8bcca97c5
tlewandowski18 <tlewandowski18@gmail.com>
2020-06-01 20:32:26 -0400

    add production env

--- knexfile.js
+++ knexfile.js
@@ -38,5 +38,25 @@ module.exports = {
 				conn.run("PRAGMA foreign_keys = ON", done) // turn on FK enforcement
 			},
 		},
-	}
+	},
+	production: {
+		client: "sqlite3",
+		useNullAsDefault: true, 
+		connection: {
+			filename: "./data/food-truck.db3",
+		},
+		migrations: {
+			directory: "./data/migrations"
+		},
+		seeds: {
+			directory: "./data/seeds"
+		},
+		// needed when using foreign keys
+		pool: {
+			afterCreate: (conn, done) => {
+				// runs after a connection is made to the sqlite engine
+				conn.run("PRAGMA foreign_keys = ON", done) // turn on FK enforcement
+			},
+		},
+	},
 }
