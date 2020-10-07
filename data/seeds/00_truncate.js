1d7fee3d24d196b12223da12fb48934b190df1e1
tlewandowski18 <tlewandowski18@gmail.com>
2020-10-07 13:01:03 -0400

    Update first seed file to use knexCleaner instead of truncate

--- data/seeds/00_truncate.js
+++ data/seeds/00_truncate.js
@@ -1,8 +1,15 @@
 
-exports.seed = async function(knex) {
-  await knex("ratings").truncate()
-  await knex("menu-items").truncate()
-  await knex("trucks").truncate()
-  await knex("operators").truncate()
-  await knex("diners").truncate()
+const knexCleaner = require("knex-cleaner");
+exports.seed = function(knex) {
+  return knexCleaner.clean(knex, {
+    ignoreTables: ["knex_migrations", "knex_migrations_lock"]
+  });
 };
+
+// exports.seed = async function(knex) {
+//   await knex("ratings").truncate()
+//   await knex("menu-items").truncate()
+//   await knex("trucks").truncate()
+//   await knex("operators").truncate()
+//   await knex("diners").truncate()
+// };
